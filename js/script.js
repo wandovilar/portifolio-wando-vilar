5f3f1503657d7f6b1c3f5e710d3a084f34b38816
wandovillar <wandovilar@outlook.com>
2026-01-16 20:52:57 -0300

    Finalização Responsividade do Projeto

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const botaoEsquerda = document.querySelector("#btnEsquerda");
-
 const botaoDireita = document.querySelector("#btnDireita");
-
 const carrossel = document.querySelector("#carrosselProjetos");
 
+function pegarPasso() {
+  const card = document.querySelector(".card-projeto");
+  const gap = 15; // mesmo gap do CSS
+  return card.offsetWidth + gap;
+}
+
 botaoDireita.addEventListener("click", () => {
-  carrossel.scrollLeft -= 400;
+  carrossel.scrollLeft += pegarPasso();
 });
 
 botaoEsquerda.addEventListener("click", () => {
-  carrossel.scrollLeft += 400;
+  carrossel.scrollLeft -= pegarPasso();
 });
-
-console.log("Esquerda:", botaoEsquerda);
-console.log("Direita:", botaoDireita);
-console.log("Carrossel:", carrossel);
